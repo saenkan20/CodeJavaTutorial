8707aaec57c12c8771955d981dec0e269b5faac4
saenkan20 <s66309010043@kktech.ac.th>
2024-07-26 14:56:42 +0700

    Create TestOperate.java

---
+++ TestOperate.java
@@ -0,0 +1,15 @@
+public class TestOperate {
+    public static void main(String args[]){
+        int x, y;
+        x = 5 + 2 * 4;
+        y = (5 + 2) * 4;
+        float value;
+        value = 1;
+        System.out.println("x = " +x);
+        System.out.println("y = " +y);
+        x = x / 3;
+        System.out.println("x = " +x);
+        value = (float)4 / 3 ;
+        System.out.println("value = " +value);
+    }
+}
