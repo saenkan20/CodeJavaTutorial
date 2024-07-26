49eea5cd79fa57786b522a056ec6959016cf260e
saenkan20 <s66309010043@kktech.ac.th>
2024-07-26 14:56:44 +0700

    Create TestNum.java

---
+++ TestNum.java
@@ -0,0 +1,12 @@
+public class TestNum {
+    public static void main(String args[]){
+        int x,y;
+        x = 10;
+        y = x+1;
+        System.out.println("x = " +x+ "y = " +y);
+        y = y++ ;
+        System.out.println("x = " +x+ "y = " +y);
+        y = ++y ;
+        System.out.println("x = " +x+ "y = " +y);
+    }   
+}
