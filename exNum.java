aa1fc626e6f169ad8132c1b33c66130d2d6822ff
saenkan20 <s66309010043@kktech.ac.th>
2024-07-26 14:56:37 +0700

    Create exNum.java

---
+++ exNum.java
@@ -0,0 +1,9 @@
+public class exNum {
+    public static void main(String[] args) {
+        int x, y;
+        x = 69;
+        y = 99;
+        int total = x+y;
+        System.out.println("total = " +total);
+    }
+}
