4374ae13f5e20e34a34b64a5dab012f0144e654d
saenkan20 <s66309010043@kktech.ac.th>
2024-07-26 14:57:11 +0700

    Create stringDemo.java

---
+++ stringDemo.java
@@ -0,0 +1,8 @@
+public class stringDemo {
+    public static void main(String[] args){
+        String greeting ;
+        greeting = "Good morning";
+        String name = "Computer";
+        System.err.println(greeting +" " +name);
+    }
+}
