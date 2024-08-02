0019728a0b11934c6f1d37c67e4c4ff289e5d36b
saenkan20 <s66309010043@kktech.ac.th>
2024-08-02 10:15:10 +0700

    Create CallName.java

---
+++ CallName.java
@@ -0,0 +1,11 @@
+public class CallName {
+     public static void main(String[] args) {
+        Name();
+    }
+
+    public static void Name() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Saenkan Snghatip");
+        }
+    }
+}
